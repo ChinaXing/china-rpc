b4e2722e401bf1ac50c74f0da92954b48bfd6fda
LambdaCat <yunxing.chen@witown.cn>
2015-08-24 12:13:27 +0800

    complete Selector

--- src/main/java/com/chinaxing/framework/rpc/model/PacketEvent.java
+++ src/main/java/com/chinaxing/framework/rpc/model/PacketEvent.java
@@ -7,7 +7,16 @@ import java.nio.ByteBuffer;
  * Created by LambdaCat on 15/8/22.
  */
 public class PacketEvent {
+    private String destination;
     private ByteBuffer buffer;
+
+    public String getDestination() {
+        return destination;
+    }
+
+    public void setDestination(String destination) {
+        this.destination = destination;
+    }
 
     public ByteBuffer getBuffer() {
         return buffer;
