9c898af931e801777bf9a0647b8089386e15e98f
LambdaCat <yunxing.chen@witown.cn>
2015-09-17 12:40:03 +0800

    ok

--- src/test/java/TestConcurrency.java
+++ src/test/java/TestConcurrency.java
@@ -1,7 +1,10 @@
 import impl.RpcPerfServiceImpl;
+import model.TestPojo;
 import service.RpcPerfService;
 
 import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Map;
 import java.util.concurrent.*;
 import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.atomic.AtomicLong;
@@ -11,10 +14,39 @@ import java.util.concurrent.atomic.AtomicLong;
  */
 public class TestConcurrency extends TestRPC {
     private String s;
+    private TestPojo pojo;
     private AtomicLong used = new AtomicLong(0);
-    private static final int CONCURRENCY = 1;
-    private static final int DATA_SIZE = 1024 * 1024 * 200;
+    private static int CONCURRENCY = 1;
+    private static int DATA_SIZE = 1024 * 1024 * 200;
     private static final AtomicInteger index = new AtomicInteger(0);
+    private static final int TEST_STR = 1;
+    private static final int TEST_POJO = 1;
+    private static int testType = TEST_STR;
+
+
+    public static void main(String[] args) {
+        if (args.length == 0) {
+            System.out.println("Miss args");
+            return;
+        }
+
+        try {
+            TestConcurrency tester = new TestConcurrency();
+            tester.testAppointCall();
+            String t = args[0];
+            if ("str".equals(t)) {
+                testType = TEST_STR;
+                return;
+            }
+            if ("pojo".equals(t)) {
+                testType = TEST_POJO;
+            }
+            tester.doTest();
+        } catch (Throwable t) {
+            t.printStackTrace();
+        }
+        System.out.println("Done");
+    }
 
     @Override
     public void doTest() throws Throwable {
@@ -42,7 +74,11 @@ public class TestConcurrency extends TestRPC {
                 return new Thread(r, "t-" + index.getAndIncrement());
             }
         });
-        buildStr(DATA_SIZE);
+        if (testType == TEST_STR) {
+            buildStr(DATA_SIZE);
+        } else if (testType == TEST_POJO) {
+            buildPojo();
+        }
         int i = CONCURRENCY;
         while (i-- > 0) {
             executor.execute(new Runnable() {
@@ -64,12 +100,31 @@ public class TestConcurrency extends TestRPC {
 
     }
 
+    private void buildPojo() {
+        pojo = new TestPojo();
+        pojo.setBool(false);
+        pojo.setIii(2233);
+        pojo.setLll(99999);
+        pojo.setListTest(Arrays.asList("hello", "world", "lambda", "cat", "china", "hangzhou"));
+        Map<String, String> map = new HashMap<>();
+        for (int i = 0; i < pojo.getListTest().size(); i += 2) {
+            map.put(pojo.getListTest().get(i), pojo.getListTest().get(i + 1));
+        }
+        pojo.setMapTest(map);
+    }
+
     private void singleTest(CyclicBarrier startBarrier, CountDownLatch latch) throws Throwable {
         RpcPerfService ts = rpc.appointRefer(RpcPerfService.class, "127.0.0.1:9119");
         startBarrier.await();
         long start = System.nanoTime();
         try {
-            ts.testStr(s);
+            if (testType == TEST_STR) {
+                ts.testStr(s);
+            } else if (testType == TEST_POJO) {
+                ts.testPojoIn(pojo);
+            } else {
+                System.out.println("Test Nothing");
+            }
             long end = System.nanoTime();
             long t = end - start;
             System.out.println(index.getAndIncrement() + " " + Thread.currentThread().getName() + " " + t);
